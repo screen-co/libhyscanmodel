051c8071fad7938bada5dd3ca1b64777007ab3cb
Alexey Sakhnov <alexsakhnov@gmail.com>
2020-08-20 18:54:55 +0300

    control-model-test: дополнен тест

--- tests/control-model-test.c
+++ tests/control-model-test.c
@@ -1,25 +1,43 @@
 #include <hyscan-control-model.h>
 #include <hyscan-dummy-device.h>
 
-#define TEST_SOURCE HYSCAN_SOURCE_SIDE_SCAN_PORT
+#define TEST_SOURCE HYSCAN_SOURCE_SIDE_SCAN_PORT           /* Источник данных. */
 #define SYNC_PERIOD (100 * G_TIME_SPAN_MILLISECOND / 1000) /* Период синхронизации параметров ГЛ. */
 #define WAIT_PERIOD (SYNC_PERIOD * 4)                      /* Период ожидания синхронизации ГЛ. */
+#define FAIL_TIMEOUT (3 * G_TIME_SPAN_SECOND)              /* Время ожидания до завершения по таймауту. */
+
+/* Параметры старта локатора. */
+#define PROJECT_NAME   "test-project"
+#define TRACK_SUFFIX   "-test-suffix"
+#define TRACK_NAME     "001"
+#define TRACK_TYPE     HYSCAN_TRACK_SURVEY
+static HyScanTrackPlan track_plan = { .start = {1, 2}, .end = {3, 4}, .speed = 5 };
 
 static HyScanControlModel *sonar_model;
 static HyScanControl *control;
 static HyScanDummyDevice *dummy_device;
+static gulong start_stop_tag;
+static guint timeout_tag;
 static GMainLoop *loop;
 
-static gboolean test_sonar         (void);
-static gboolean test_sensor        (void);
-static gboolean test_param         (void);
-static gboolean test_atvg_sync     (void);
-static gboolean test_sync_failed   (void);
-static gboolean test_sync_happened (void);
-static void     check_params       (HyScanParam     *param,
-                                    HyScanParamList *expect_list);
+static gboolean test_sonar                 (gpointer               data);
+static gboolean test_sensor                (gpointer               data);
+static gboolean test_start                 (gpointer               data);
+static gboolean test_param                 (gpointer               data);
+static gboolean test_atvg_sync             (gpointer               data);
+static gboolean test_sync_failed           (gpointer               data);
+static gboolean test_sync_happened         (gpointer               data);
+static void     test_stopped               (void);
+static void     test_started               (void);
+static void     test_control_model_started (void);
+static gboolean track_plan_equal           (const HyScanTrackPlan *plan1,
+                                            const HyScanTrackPlan *plan2);
+static gboolean fail_by_timeout            (gpointer               data);
+static void     check_params               (HyScanParam           *param,
+                                            HyScanParamList       *expect_list);
 
 
+/* Сравнивает значения двух HyScanParam. */
 static void
 check_params (HyScanParam     *param,
               HyScanParamList *expect_list)
@@ -52,29 +70,10 @@ check_params (HyScanParam     *param,
   g_object_unref (read_plist);
 }
 
+/* Тест интерфейса HyScanParam. */
 static gboolean
-test_param (void)
+test_param (gpointer data)
 {
-  // {
-  //   HyScanDataSchema *schema;
-  //   schema = hyscan_param_schema (HYSCAN_PARAM (sonar));
-  //   const gchar *const *keys;
-  //   gint i;
-  //   keys = hyscan_data_schema_list_keys (schema);
-  //   for (i = 0; keys[i] != NULL; i++)
-  //     {
-  //       HyScanDataSchemaKeyAccess access = hyscan_data_schema_key_get_access (schema, keys[i]);
-  //
-  //       g_print ("Key %s: %c%c%c\n",
-  //                keys[i],
-  //                (access & HYSCAN_DATA_SCHEMA_ACCESS_READ) ? 'R' : ' ',
-  //                (access & HYSCAN_DATA_SCHEMA_ACCESS_WRITE) ? 'W' : ' ',
-  //                (access & HYSCAN_DATA_SCHEMA_ACCESS_HIDDEN) ? 'H' : ' '
-  //                );
-  //     }
-  //   g_object_unref (schema);
-  // }
-
   HyScanParamList *after, *before;
   gchar *param_key, *system_key;
   const gchar *prefix;
@@ -106,27 +105,13 @@ test_param (void)
   g_object_unref (after);
   g_object_unref (before);
 
-  g_main_quit (loop);
+  g_idle_add ((GSourceFunc) test_start, NULL);
 
   return G_SOURCE_REMOVE;
 }
 
-/* Шаги проверки локатора и его состояния. */
-#define TEST_SONAR(pm, set, check, get, test)       \
-  G_STMT_START {                                    \
-    g_message ("Test sonar param: %s", pm);         \
-    if (!(set))                                     \
-      g_error ("Failed to set %s", (pm));           \
-    if (!(check))                                   \
-      g_error ("Failed to check %s", (pm));         \
-    if (!(get))                                     \
-      g_error ("Failed to get state %s", (pm));     \
-    if (!(test))                                    \
-      g_error ("State of %s is incorrect", (pm));   \
-  } G_STMT_END
-
 static gboolean
-test_sensor (void)
+test_sensor (gpointer data)
 {
   HyScanSensor *sensor = HYSCAN_SENSOR (sonar_model);
   HyScanSensorState *state = HYSCAN_SENSOR_STATE (sonar_model);
@@ -160,7 +145,7 @@ test_sensor (void)
 }
 
 static gboolean
-test_sync_failed (void)
+test_sync_failed (gpointer data)
 {
   /* Проверяем, что состояние ещё не синхронизировано. */
   if (hyscan_dummy_device_check_sync (dummy_device))
@@ -171,8 +156,22 @@ test_sync_failed (void)
   return G_SOURCE_REMOVE;
 }
 
+/* Шаги проверки локатора и его состояния. */
+#define TEST_SONAR(pm, set, check, get, test)       \
+  G_STMT_START {                                    \
+    g_message ("Test sonar param: %s", pm);         \
+    if (!(set))                                     \
+      g_error ("Failed to set %s", (pm));           \
+    if (!(check))                                   \
+      g_error ("Failed to check %s", (pm));         \
+    if (!(get))                                     \
+      g_error ("Failed to get state %s", (pm));     \
+    if (!(test))                                    \
+      g_error ("State of %s is incorrect", (pm));   \
+  } G_STMT_END
+
 static gboolean
-test_sync_happened (void)
+test_sync_happened (gpointer data)
 {
   HyScanSonar *sonar = HYSCAN_SONAR (sonar_model);
   HyScanSonarState *state = HYSCAN_SONAR_STATE (sonar_model);
@@ -228,7 +227,7 @@ test_sync_happened (void)
 }
 
 static gboolean
-test_atvg_sync (void)
+test_atvg_sync (gpointer data)
 {
   HyScanSonar *sonar = HYSCAN_SONAR (sonar_model);
   static guint iteration = 0;
@@ -251,7 +250,7 @@ test_atvg_sync (void)
 }
 
 static gboolean
-test_sonar (void)
+test_sonar (gpointer data)
 {
   HyScanSonar *sonar = HYSCAN_SONAR (sonar_model);
   HyScanSonarState *state = HYSCAN_SONAR_STATE (sonar_model);
@@ -292,15 +291,191 @@ test_sonar (void)
   return G_SOURCE_REMOVE;
 }
 
+static gboolean
+fail_by_timeout (gpointer data)
+{
+  g_error ("Failed by timeout");
+  
+  return G_SOURCE_REMOVE;
+}
+
+static gboolean
+track_plan_equal (const HyScanTrackPlan *plan1,
+                  const HyScanTrackPlan *plan2)
+{
+  return ABS (plan1->speed - plan2->speed) < 1e-3 &&
+         ABS (plan1->start.lat - plan2->start.lat) < 1e-3 &&
+         ABS (plan1->start.lon - plan2->start.lon) < 1e-3 &&
+         ABS (plan1->end.lat - plan2->end.lat) < 1e-3 &&
+         ABS (plan1->end.lon - plan2->end.lon) < 1e-3;
+}
+
+static void
+test_started (void)
+{
+  gchar *project_name;
+  gchar *track_name;
+  HyScanTrackType track_type;
+  HyScanTrackPlan *plan;
+  gboolean success;
+
+  g_message ("\"start-stop\" signal emitted");
+
+  /* Отключаем ошибку по таймауту. */
+  g_source_remove (timeout_tag);
+
+  /* Отключаем текущий обработчик сигнала. */
+  g_signal_handler_disconnect (sonar_model, start_stop_tag);
+
+  /* Проверяем, что локатор работает с нужными параметрами. */
+  if (!hyscan_sonar_state_get_start (HYSCAN_SONAR_STATE (sonar_model), &project_name, &track_name, &track_type, &plan))
+    g_error ("Sonar has not started");
+
+  success = g_strcmp0 (PROJECT_NAME, project_name) == 0 &&
+            g_strcmp0 (TRACK_NAME, track_name) == 0 &&
+            track_type == TRACK_TYPE &&
+            track_plan_equal (plan, &track_plan);
+
+  g_message ("Sonar has started track \"%s/%s\" (type = %d, plan = %d)", project_name, track_name, track_type, plan != NULL);
+
+  if (!success)
+    g_error ("Sonar started with wrong parameters");
+
+  hyscan_track_plan_free (plan);
+
+  /* Проверка остановки локатора. */
+  g_message ("Test hyscan_sonar stop. Waiting for \"start-stop\" signal...");
+  timeout_tag = g_timeout_add (FAIL_TIMEOUT, (GSourceFunc) fail_by_timeout, NULL);
+  start_stop_tag = g_signal_connect_swapped (sonar_model, "start-stop",
+                                             G_CALLBACK (test_stopped),
+                                             GUINT_TO_POINTER(timeout_tag));
+  hyscan_sonar_stop (HYSCAN_SONAR (sonar_model));
+}
+
+static void
+test_stopped (void)
+{
+  g_message ("\"start-stop\" signal emitted");
+
+  /* Отключаем ошибку по таймауту. */
+  g_source_remove (GPOINTER_TO_UINT (timeout_tag));
+
+  /* Отключаем текущий обработчик сигнала. */
+  g_signal_handler_disconnect (sonar_model, start_stop_tag);
+  
+  if (hyscan_sonar_state_get_start (HYSCAN_SONAR_STATE (sonar_model), NULL, NULL, NULL, NULL))
+    g_error ("Failed to stop sonar");
+
+  /* Проверка запуска локатора. */
+  g_message ("Test hyscan_sonar stop. Waiting for \"start-stop\" signal...");
+  timeout_tag = g_timeout_add (FAIL_TIMEOUT, (GSourceFunc) fail_by_timeout, NULL);
+  start_stop_tag = g_signal_connect_swapped (sonar_model, "start-stop",
+                                             G_CALLBACK (test_control_model_started),
+                                             GUINT_TO_POINTER (timeout_tag));
+  hyscan_control_model_set_track_sfx (sonar_model, TRACK_SUFFIX, TRUE);
+  hyscan_control_model_set_track_name (sonar_model, NULL);
+  hyscan_control_model_set_project (sonar_model, PROJECT_NAME);
+  hyscan_control_model_start (sonar_model);
+}
+
+static void
+test_control_model_started (void)
+{
+  gchar *track_name;
+  gchar *project_name;
+  HyScanTrackPlan *plan;
+  HyScanTrackType track_type;
+  gboolean success;
+  
+  g_message ("\"start-stop\" signal emitted");
+
+  /* Отключаем ошибку по таймауту. */
+  g_source_remove (GPOINTER_TO_UINT (timeout_tag));
+
+  /* Отключаем текущий обработчик сигнала. */
+  g_signal_handler_disconnect (sonar_model, start_stop_tag);
+
+  if (!hyscan_sonar_state_get_start (HYSCAN_SONAR_STATE (sonar_model), &project_name, &track_name, &track_type, &plan))
+    g_error ("Failed to start sonar");
+
+  g_message ("Sonar has started track \"%s/%s\" (type = %d, plan = %d)",
+             project_name, track_name, track_type, plan != NULL);
+
+  success = g_strcmp0 (project_name, PROJECT_NAME) == 0 &&
+            g_str_has_suffix (track_name, TRACK_SUFFIX) &&
+            track_type == TRACK_TYPE &&
+            plan == NULL;
+
+  if (!success)
+    g_error ("Sonar started with wrong parameters");
+  
+  g_main_quit (loop);
+}
+
+static gboolean
+test_start (gpointer data)
+{
+  g_message ("Test hyscan_sonar_start. Waiting for \"start-stop\" signal...");
+
+  timeout_tag = g_timeout_add (FAIL_TIMEOUT, (GSourceFunc) fail_by_timeout, NULL);
+  start_stop_tag = g_signal_connect_swapped (sonar_model, "start-stop",
+                                             G_CALLBACK (test_started),
+                                             GUINT_TO_POINTER (timeout_tag));
+  
+  hyscan_sonar_start (HYSCAN_SONAR (sonar_model), PROJECT_NAME, TRACK_NAME, TRACK_TYPE, &track_plan);
+  
+  return G_SOURCE_REMOVE;
+}
+
 int
 main (int    argc,
       char **argv)
 {
+  HyScanDB *db;
+  gchar *db_uri;
+
+  {
+    gchar **args;
+    GError *error = NULL;
+    GOptionContext *context;
+
+#ifdef G_OS_WIN32
+    args = g_win32_get_command_line ();
+#else
+    args = g_strdupv (argv);
+#endif
+
+    context = g_option_context_new ("<db-uri>");
+    g_option_context_set_help_enabled (context, TRUE);
+    g_option_context_set_ignore_unknown_options (context, FALSE);
+    if (!g_option_context_parse_strv (context, &args, &error))
+      {
+        g_print ("%s\n", error->message);
+        return -1;
+      }
+
+    if (g_strv_length (args) != 2)
+      {
+        g_print ("%s", g_option_context_get_help (context, FALSE, NULL));
+        return -1;
+      }
+
+    g_option_context_free (context);
+
+    db_uri = g_strdup (args[1]);
+    g_strfreev (args);
+  }
+
+  db = hyscan_db_new (db_uri);
+  if (db == NULL)
+    g_error ("Failed to connect to db: %s", db_uri);
+
   dummy_device = hyscan_dummy_device_new (HYSCAN_DUMMY_DEVICE_SIDE_SCAN);
 
   control = hyscan_control_new ();
   hyscan_control_device_add (control, HYSCAN_DEVICE (dummy_device));
   hyscan_control_device_bind (control);
+  hyscan_control_writer_set_db (control, db);
 
   sonar_model = hyscan_control_model_new (control);
   hyscan_control_model_set_sync_timeout (sonar_model, SYNC_PERIOD);
@@ -310,6 +485,10 @@ main (int    argc,
   g_idle_add ((GSourceFunc) test_sensor, NULL);
   g_main_run (loop);
 
+  hyscan_db_project_remove (db, PROJECT_NAME);
+
+  g_free (db_uri);
+  g_object_unref (db);
   g_object_unref (control);
   g_object_unref (dummy_device);
   g_object_unref (sonar_model);
